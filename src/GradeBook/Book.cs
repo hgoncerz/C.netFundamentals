ac676054902980dba8d3bce2987edb2213bc43f8
hgoncerz <hubertg489655@gmail.com>
2022-08-10 12:10:45 +0200

    try catch and throw and final

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -43,7 +43,7 @@ namespace GradeBook
             }
             else
             {
-                Console.WriteLine("Invali value");
+                throw new ArgumentException($"invalid {nameof(grade)}");
             }
             
         }
@@ -62,6 +62,26 @@ namespace GradeBook
             }
 
             result.Average /= grades.Count;
+
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+
+            }
 
             return result;
         }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,15 +8,39 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Book book = new Book("Scott's Grade book"); 
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            Book book = new Book("Scott's Grade book");
+         
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                String input = Console.ReadLine();
+                if(input == "q")
+                {
+                    break;
+                }
+                try
+                {
+                    double grade = Convert.ToDouble(input);
+                    book.AddGrade(grade);
+                }
+                catch (AggregateException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+              
+
+            }
+
             var stats = book.GetStatistic();
 
              Console.WriteLine($"The average grade is {stats.Average:n1}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Low}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
             Console.ReadKey();
         }
     }
